d3e3d1086cb1d1d7a318ca2a37cf174c10907292
unknown <rezwanul.haquebd777gmail.com>
2023-07-16 00:02:26 +0600

    todays work [done]

--- Backend/Models/User_model.js
+++ Backend/Models/User_model.js
@@ -1,34 +1,53 @@
-const  mongoose = require("mongoose");
-const authData=mongoose.Schema({
 
-fastName:{
-    type:String,
-    require:true
-},
-lastName:{
-    type:String,
-    require:true,
-},
-poneNumber:{
-    type:String,
-    unique:true,
-    require:true,
-},
-email:{
-    type:String,
-    unique:true,
-    require:true,
-},
-password:{
-    type:String
-},
-createDate:{
-    type:Date, 
-    default:Date.now()
-}
 
-},versionKey=false);
+//third-parity module require
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
 
-const userModel = mongoose.model('author',authData)
+//user schema
+const userSchema  = new Schema({
+    firstName:{
+        type:String,
+        require:true
+    },
+    lastName:{
+        type:String,
+        require:true,
+    },
+    phoneNumber:{
+        type:String,
+        unique:true,
+        require:true,
+    },
+    email:{
+        type: String,
+        trim: true,
+        required: [true, "Please add email"],
+        unique: true,
+        match: [/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i, "please enter a valid emile"],
+    },
+    password:{
+        type: String,
+        required: [true, "password is Required!"],
+        trim: true,
+        minLength: [6, "password must be up 6 creacters"],
+    },
+    role:{
+        type: Number,
+        trim: true,
+        default: 0,
+    },
+    createDate:{
+        type:Date, 
+        default:Date.now()
+    }
+},
+{
+    timestamps:true,
+    versionKey:false
+});
 
-module.exports=userModel;
+
+const User = mongoose.model('users', userSchema);
+//customer Schema exports
+module.exports = User;
